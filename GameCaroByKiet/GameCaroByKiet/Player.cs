70e26597646a3ed57a5b67b9427ae7078b5174d0
Tuấn Kiệt (Bee) <91370924+Rain0809@users.noreply.github.com>
2022-12-25 19:59:04 +0700

    Add files via upload

---
+++ GameCaroByKiet/GameCaroByKiet/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaroByKiet
+{
+    public class Player
+    {
+        private string name;    // Ctrl + R + E
+        private Image mark;
+
+
+        public string Name { get => name; set => name = value; }
+        public Image Mark { get => mark; set => mark = value; }
+
+        public Player(string name, Image mark)
+        {
+            this.Name = name;
+            this.Mark = mark;
+        }
+    }
+}
