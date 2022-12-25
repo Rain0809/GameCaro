df38b09c79a214aa3c7dfe9c395472aca063e4e2
Tuấn Kiệt (Bee) <91370924+Rain0809@users.noreply.github.com>
2022-12-25 17:35:25 +0700

    Add files via upload

---
+++ GameCaroByKiet/GameCaroByKiet/ChessBoardCaro.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCaroByKiet
+{
+    public partial class ChessBoardCaro : Form
+    {
+        #region Properties
+        ChessBoardManager ChessBoard;
+
+        SocketManager socket;
+        #endregion
+
+        #region Initialize
+        public ChessBoardCaro()
+        {
+            InitializeComponent();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            ChessBoard = new ChessBoardManager(pnlChessBoard, txbPlayerName, pctbMark);
+            ChessBoard.EndedGame += ChessBoard_EndedGame;
+            ChessBoard.PlayerMarked += ChessBoard_PlayerMarked;
+
+            prcbCoolDown.Step = Cons.COOL_DOWN_STEP;
+            prcbCoolDown.Maximum = Cons.COOL_DOWN_TIME;
+            prcbCoolDown.Value = 0;
+
+            tmCoolDown.Interval = Cons.COOL_DOWN_INTERVAL;
+
+            socket = new SocketManager();
+
+            NewGame();
+        }
+        #endregion
+
+        #region Methods
+
+        void EndGame()
+        {
+            tmCoolDown.Stop();
+            pnlChessBoard.Enabled = false;
+        }
+
+        void NewGame()
+        {
+            prcbCoolDown.Value = 0;
+            tmCoolDown.Stop();
+            ChessBoard.DrawChessBoard();
+        }
+
+        void Quit()
+        {
+            Application.Exit();
+        }
+
+        void ChessBoard_PlayerMarked(object sender, ButtonClickEvent e)
+        {
+            tmCoolDown.Start();
+            pnlChessBoard.Enabled = false;
+            prcbCoolDown.Value = 0;
+
+            socket.Send(new SocketData((int)SocketCommand.SEND_POINT, "", e.ClickedPoint));
+
+            Listen();
+        }
+
+        void ChessBoard_EndedGame(object sender, EventArgs e)
+        {
+            EndGame();
+            socket.Send(new SocketData((int)SocketCommand.END_GAME, "", new Point()));
+        }
+
+        private void tmCoolDown_Tick(object sender, EventArgs e)
+        {
+            prcbCoolDown.PerformStep();
+
+            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
+            {
+                EndGame();
+                socket.Send(new SocketData((int)SocketCommand.TIME_OUT, "", new Point()));
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGame();
+            socket.Send(new SocketData((int)SocketCommand.NEW_GAME, "", new Point()));
+            pnlChessBoard.Enabled = true;
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Quit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có chắc muốn thoát", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
+            {
+                e.Cancel = true;
+            }
+            else
+            {
+                try
+                {
+                    socket.Send(new SocketData((int)SocketCommand.QUIT, "", new Point()));
+                }
+                catch { }
+            }
+        }
+
+        private void btnLAN_Click(object sender, EventArgs e)
+        {
+            socket.IP = txbIP.Text;
+
+            if (!socket.ConnectServer())
+            {
+                socket.isServer = true;
+                pnlChessBoard.Enabled = true;
+                socket.CreateServer();
+            }
+            else
+            {
+                socket.isServer = false;
+                pnlChessBoard.Enabled = false;
+                Listen();
+            }
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Wireless80211);
+
+            if (string.IsNullOrEmpty(txbIP.Text))
+            {
+                txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Ethernet);
+            }
+        }
+
+        void Listen()
+        {
+            Thread listenThread = new Thread(() =>
+            {
+                try
+                {
+                    SocketData data = (SocketData)socket.Receive();
+
+                    ProcessData(data);
+                }
+                catch
+                {
+                }
+            });
+            listenThread.IsBackground = true;
+            listenThread.Start();
+        }
+
+        private void ProcessData(SocketData data)
+        {
+            switch (data.Command)
+            {
+                case (int)SocketCommand.NOTIFY:
+                    MessageBox.Show(data.Message);
+                    break;
+                case (int)SocketCommand.NEW_GAME:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        NewGame();
+                        pnlChessBoard.Enabled = false;
+                    }));
+                    break;
+                case (int)SocketCommand.SEND_POINT:
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        prcbCoolDown.Value = 0;
+                        pnlChessBoard.Enabled = true;
+                        tmCoolDown.Start();
+                        ChessBoard.OtherPlayerMark(data.Point);
+                    }));
+                    break;
+                case (int)SocketCommand.END_GAME:
+                    MessageBox.Show("Đã 5 con trên 1 hàng");
+                    break;
+                case (int)SocketCommand.TIME_OUT:
+                    MessageBox.Show("Hết giờ");
+                    break;
+                case (int)SocketCommand.QUIT:
+                    tmCoolDown.Stop();
+                    MessageBox.Show("Người chơi đã thoát");
+                    break;
+                default:
+                    break;
+            }
+
+            Listen();
+        }
+
+        #endregion
+    }
+}
---
+++ GameCaroByKiet/GameCaroByKiet/PlayInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaroByKiet
+{
+    public class PlayInfo
+    {
+        private Point point;
+        private int currentPlayer;
+
+        public Point Point { get => point; set => point = value; }
+        public int CurrentPlayer { get => currentPlayer; set => currentPlayer = value; }
+
+        public PlayInfo(Point point, int currentPlayer)
+        {
+            this.Point = point;
+            this.CurrentPlayer = currentPlayer;
+        }     
+    }
+}
